9fcc2942f6a2040b4d97f6fb871a47c05c53079a
Aisha <aishalatif121@gmail.com>
2024-01-27 19:38:39 -0500

    Buttons

--- pet_pals/lib/main.dart
+++ pet_pals/lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,3 +19,75 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+*/
+import 'package:flutter/material.dart';
+import 'Title.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => MyAppState(),
+//       child: MaterialApp(
+//         title: 'Namer App',
+//         theme: ThemeData(
+//           useMaterial3: true,
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+//         ),
+//         home: MyHomePage (),
+//       ),
+//     );
+//   }
+// }
+
+// class MyAppState extends ChangeNotifier {
+//   var current = WordPair.random();
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: const Color.fromARGB(255, 189, 80, 117),
+//           title: Text('Pet Pals'),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: () {
+//                   print('Sign In button pressed!');
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   fixedSize: Size(200, 50),
+//                   backgroundColor: Colors.green, // Change the background color
+//                 ),
+//                 child: Text('Sign In'),
+//               ),
+//               SizedBox(height: 16),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   print('Create Account button pressed!');
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   fixedSize: Size(200, 50),
+//                   backgroundColor: Colors.blue, // Change the background color
+//                 ),
+//                 child: Text('Create Account'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
